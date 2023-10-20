533c6a0cc520774fbe3ab8bdbbb124b2e8a53a63
ItaloRuann21 <italotavares751@gmail.com>
2023-10-20 01:58:08 -0300

    Adicionado requisições POST com api formspree.

--- js/agendar.js
+++ js/agendar.js
@@ -1,19 +1,19 @@
-const tipoServico = localStorage.getItem("servicosimples.title");
-const data = localStorage.getItem("lavajato-data");
-const hora = localStorage.getItem("lavajato-hora");
+const tipoServico = localStorage.getItem("servicosimples.title")
+const data = localStorage.getItem("lavajato-data")
+const hora = localStorage.getItem("lavajato-hora")
 
 if (tipoServico && data && hora) {
   // Use && em vez de &
-  const mensagemPreSelecionada = `Olá, eu quero agendar meu serviço:\n*Tipo de serviço:* ${tipoServico}\n*Data marcada:* ${data}\n*Horário marcado:* ${hora}`;
+  const mensagemPreSelecionada = `Olá, eu quero agendar meu serviço:\n*Tipo de serviço:* ${tipoServico}\n*Data marcada:* ${data}\n*Horário marcado:* ${hora}`
 
   // Substitua o número de telefone abaixo pelo número correto
-  const numeroTelefone = "557996541447";
+  const numeroTelefone = "557996541447"
 
-  const mensagemCodificada = encodeURIComponent(mensagemPreSelecionada);
+  const mensagemCodificada = encodeURIComponent(mensagemPreSelecionada)
 
-  const linkWhatsApp = `https://api.whatsapp.com/send?phone=${numeroTelefone}&text="${mensagemCodificada}"`;
-  const linkElement = document.getElementById("btn3");
-  linkElement.href = linkWhatsApp;
+  const linkWhatsApp = `https://api.whatsapp.com/send?phone=${numeroTelefone}&text="${mensagemCodificada}"`
+  const linkElement = document.getElementById("btn3")
+  linkElement.href = linkWhatsApp
 } else {
-  console.log("Dados necessários não estão definidos no localStorage.");
+  console.log("Dados necessários não estão definidos no localStorage.")
 }
--- js/servicos.js
+++ js/servicos.js
@@ -1,11 +1,13 @@
-const servicosCards = document.querySelectorAll(".card");
+const servicosCards = document.querySelectorAll(".card")
 
 servicosCards.forEach((card) => {
-  const button = card.querySelector(".btn");
+  const button = card.querySelector(".btn")
   button.addEventListener("click", function () {
-    const title = card.querySelector(".card-title");
-    localStorage.setItem("servicosimples.title", title.textContent);
-  });
-});
+    const title = card.querySelector(".card-title")
+    // Adiciona a verificação para evitar o erro de título nulo
+    const titleText = title ? title.textContent : ""
+    localStorage.setItem("servicosimples.title", titleText)
+  })
+})
 
 // window.location.replace("http://127.0.0.1:5500/Pages/Cadastro.html");
