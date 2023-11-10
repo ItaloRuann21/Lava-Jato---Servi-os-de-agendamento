65cf7066a1b6b9b12ae32e523be5b099d801b050
ItaloRuann21 <italotavares751@gmail.com>
2023-11-10 19:43:31 -0300

    finalizado

--- js/agendar.js
+++ js/agendar.js
@@ -1,17 +1,17 @@
-function Agendar(){
+function Agendar() {
   const tipoServico = localStorage.getItem("servicosimples.title")
   const data = localStorage.getItem("lavajato-data")
   const hora = localStorage.getItem("lavajato-hora")
-  
+
   if (tipoServico && data && hora) {
     // Use && em vez de &
     const mensagemPreSelecionada = `Olá, eu quero agendar meu serviço:\n*Tipo de serviço:* ${tipoServico}\n*Data marcada:* ${data}\n*Horário marcado:* ${hora}`
-  
+
     // Substitua o número de telefone abaixo pelo número correto
-    const numeroTelefone = "557996541447"
-  
+    const numeroTelefone = "5579999506852"
+
     const mensagemCodificada = encodeURIComponent(mensagemPreSelecionada)
-  
+
     const linkWhatsApp = `https://api.whatsapp.com/send?phone=${numeroTelefone}&text="${mensagemCodificada}"`
     const linkElement = document.getElementById("btn3")
     linkElement.href = linkWhatsApp
